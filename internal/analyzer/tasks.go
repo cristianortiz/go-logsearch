e98c6a996c4b4cd1c3e9e4f89d2027497cec3614
cristianortiz <cristian.ortiznavia82@gmail.com>
2025-08-15 21:21:19 -0400

    [FIX] pipeline logic more explicit using more functions to implements the concurrent pipeline stages, also using context to add controlled cancelations to gourtines amongs others benefits

--- internal/analyzer/tasks.go
+++ internal/analyzer/tasks.go
@@ -8,14 +8,22 @@ import (
 	"go.uber.org/zap"
 )
 
+// represents the content of a readed file
+type FileContent struct {
+	FileName string
+	Content  string
+}
+
+// handles only reading files
 type ReadFileTask struct {
 	FilePath      string
-	ProcessorPool *workerpool.WorkerPool
+	OutputChannel chan<- FileContent
 	ReadFileWg    *sync.WaitGroup
 }
 
 type ProcessChunkTask struct {
-	Content string
+	Content  string
+	FileName string
 }
 
 func (r *ReadFileTask) Execute() (workerpool.Result, error) {
@@ -25,15 +33,21 @@ func (r *ReadFileTask) Execute() (workerpool.Result, error) {
 	if err != nil {
 		return ReadFileResult{}, err
 	}
-	//send processChunkTask to processorPool
-	r.ProcessorPool.Submit(ProcessChunkTask{Content: string(content)})
-	return ReadFileResult{}, nil
+	//send content to outputChannel
+	r.OutputChannel <- FileContent{
+		FileName: r.FilePath,
+		Content:  string(content),
+	}
+	return ReadFileResult{FilePath: r.FilePath}, nil
 
 }
 
 func (t ProcessChunkTask) Execute() (workerpool.Result, error) {
 	log.Debug("Executing ProcessChunkTask")
+
 	result := AnalyzeFileContent(t.Content)
+	result.FileName = t.FileName
 	log.Debug("ProcessChunkTask finished", zap.Int("error_count", result.ErrorCount))
+
 	return ProcessChunkResult{Result: result}, nil
 }
--- main.go
+++ main.go
@@ -32,48 +32,36 @@ func main() {
 
 	//-- Channels to goroutines communications --
 	//channel for file routes
-	filePaths := make(chan string, 10)
+	filePaths := make(chan string, 10)                  //step 1 -> step 2
+	fileContents := make(chan analyzer.FileContent, 10) //step 2 -> step 3
+
 	//-- WorkerPools--
 	readerPool := workerpool.NewWorkerPool(numReaderWorkers)
 	processorPool := workerpool.NewWorkerPool(numReaderWorkers)
+
 	//-- Launch workerpools
 	go readerPool.Run(ctx)
 	go processorPool.Run(ctx)
 
-	// launch goroutines to monitoring readerPool and processorPool errors
-	go func() {
-		for err := range readerPool.Errors() {
-			log.Error("readPool error detected", zap.Error(err))
-			// critical error cancel ops
-			if isCriticalError(err) {
-				log.Error("critical cancel operation")
-				cancel()
-				return
-			}
-		}
-	}()
-	go func() {
-		for err := range processorPool.Errors() {
-			log.Error("processorPool error detected", zap.Error(err))
-			if isCriticalError(err) {
-				log.Error("critical cancel operation")
-				cancel()
-				return
-			}
-		}
-	}()
+	//monitoring errors
+	errorMonitor(cancel, &readerPool, &processorPool)
 
 	//--waitGroups to sync
 	var discoverWg sync.WaitGroup
 	var readFileWg sync.WaitGroup
 	var processingWg sync.WaitGroup
 
+	//--step 1 discover files
 	discoverWg.Add(1)
-	go discoverFiles(&discoverWg, filePaths)
+	go discoverFiles(ctx, &discoverWg, filePaths)
 
-	// Procesing files (consumer)
+	//--step 2 read files
+	readFileWg.Add(1)
+	go readFiles(ctx, &readFileWg, filePaths, fileContents, &readerPool)
+
+	//--step 3 process content
 	processingWg.Add(1)
-	go processFiles(&processingWg, &readFileWg, filePaths, &readerPool, &processorPool)
+	go processContents(ctx, &processingWg, fileContents, &processorPool)
 	processingWg.Wait()
 
 	collectAndDisplayResults(&processorPool)
@@ -81,54 +69,97 @@ func main() {
 }
 
 // discoverFiles search files with .log extension inside directory and send it to jobs channel
-func discoverFiles(wg *sync.WaitGroup, filePaths chan<- string) {
+func discoverFiles(ctx context.Context, wg *sync.WaitGroup, filePaths chan<- string) {
 	log.Debug("Entering discoverfiles goroutine")
 
 	defer wg.Done()
 	defer close(filePaths)
 
 	err := filepath.WalkDir(logDir, func(path string, info os.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && filepath.Ext(path) == ".log" {
-			log.Debug("File founded, sending to jobs", zap.String("file", path))
-			filePaths <- path
-		}
-		return nil
-	})
+		select {
+		//check cancelation signal in everty iteration
+		case <-ctx.Done():
+			return ctx.Err() //stop the walkdir if a context cancelation signal was received
 
-	if err != nil {
+		//continue normal proccesing
+		default:
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() && filepath.Ext(path) == ".log" {
+				log.Debug("File found, sending to channel", zap.String("file", path))
+				//send to channel but also check for cancelation signal
+				select {
+				case filePaths <- path: //file sended correctly
+					//context canceled while sending is executed
+				case <-ctx.Done():
+					return ctx.Err()
+
+				}
+			}
+			return nil
+		}
+	})
+	//check if the err is 'normal', about the walkdir logic only, a context signal cancelation is not an err per se
+	if err != nil && err != context.Canceled {
 		log.Error("Error searching for files", zap.Error(err))
 	}
 
+	log.Info("File discovery completed")
 }
 
-func processFiles(processingWg *sync.WaitGroup, readFileWg *sync.WaitGroup,
-	filePaths <-chan string, readerPool *workerpool.WorkerPool,
-	processorPool *workerpool.WorkerPool) {
+func readFiles(ctx context.Context, wg *sync.WaitGroup, filePaths <-chan string, fileContents chan<- analyzer.FileContent, readerPool *workerpool.WorkerPool) {
+	defer wg.Done()
+	log.Debug("Starting file reading stage")
 
-	defer processingWg.Done()
-	log.Debug("Entering processFiles goroutine")
-
+	var tasksWg sync.WaitGroup
+	//process every file path
 	for filePath := range filePaths {
-		readFileWg.Add(1)
-		log.Debug("Submitting ReadFileTask", zap.String("file", filePath))
-
-		//creates and send read task
-		readFileTask := &analyzer.ReadFileTask{
-			FilePath:      filePath,
-			ProcessorPool: processorPool,
-			ReadFileWg:    readFileWg,
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			tasksWg.Add(1)
+			log.Debug("Submitting ReadFileTask", zap.String("file", filePath))
+			//creates and send reads tasks
+			readFileTask := &analyzer.ReadFileTask{
+				FilePath:      filePath,
+				OutputChannel: fileContents,
+				ReadFileWg:    &tasksWg,
+			}
+			readerPool.Submit(readFileTask)
 		}
-		readerPool.Submit(readFileTask)
 	}
-	// waits for all readers task are finished
-	readFileWg.Wait()
-	log.Info("All read tasks completed, stopping readerPool")
-	readerPool.Stop()
-	//waits for all chunks are processed
 
+	//waits for all reader task are finished
+	tasksWg.Wait()
+	//close channel qhen all reads are done
+	close(fileContents)
+	log.Info("All files have need read")
+	readerPool.Stop()
+
+}
+
+func processContents(ctx context.Context, wg *sync.WaitGroup, fileContents <-chan analyzer.FileContent, processorPool *workerpool.WorkerPool) {
+	defer wg.Done()
+	log.Debug("Starting content processing stage")
+
+	//process every content fragment
+	for content := range fileContents {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			log.Debug("Submittin ProcessChunkTask", zap.String("file", content.FileName))
+			//creates and send process tasks
+			processTask := analyzer.ProcessChunkTask{
+				Content:  content.Content,
+				FileName: content.FileName,
+			}
+			processorPool.Submit(processTask)
+		}
+	}
+	log.Info("All content has been processed")
 	processorPool.Stop()
 }
 
@@ -141,18 +172,18 @@ func collectAndDisplayResults(processorPool *workerpool.WorkerPool) {
 			log.Error("Error processing chunk", zap.Error(r.Err()))
 			continue
 		}
-		// Obtener el resultado usando el nuevo método específico
 		analysisResult := r.GetAnalysisResult()
 		if analysisResult != nil {
 			totalErrorCount += analysisResult.ErrorCount
 			filesAnalyzed++
 			log.Debug("Processed file",
+				zap.String("file", analysisResult.FileName),
 				zap.Int("errors", analysisResult.ErrorCount),
 				zap.Int("total_so_far", totalErrorCount))
 		}
 	}
 
-	// Mostrar resultados
+	// show results
 	log.Info("-- Concurrent Analysis finished --",
 		zap.Int("files_analyzed", filesAnalyzed),
 		zap.Int("total_errors_found", totalErrorCount),
@@ -168,8 +199,35 @@ func collectAndDisplayResults(processorPool *workerpool.WorkerPool) {
 	fmt.Println("======================================")
 }
 
+func errorMonitor(cancel context.CancelFunc, readerPool, processorPool *workerpool.WorkerPool) {
+	// readerPool monitoring errors
+	go func() {
+		for err := range readerPool.Errors() {
+			log.Error("ReaderPool error", zap.Error(err))
+			if isCriticalError(err) {
+				log.Error("Critical error in reader pool, cancelling operation")
+				cancel()
+				return
+			}
+		}
+	}()
+	// processorPool monitoring errors
+	go func() {
+		for err := range processorPool.Errors() {
+			log.Error("ProcessorPool error", zap.Error(err))
+			if isCriticalError(err) {
+				log.Error("Critical error in processor pool, cancelling operation")
+				cancel()
+				return
+			}
+		}
+	}()
+}
+
 func isCriticalError(err error) bool {
 
 	// logic to define if an error is critical for the app operation  ex. directory or file access, disc related errors, etc
+	// for now just log the error
+	log.Error("critital error", zap.Error(err))
 	return false
 }
